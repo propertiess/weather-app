9a2a587bf52f37f30346716ead3467145ab3aff9
propertiess <erofeev2001@yandex.ru>
2023-02-05 21:47:41 +0300

    chore: add react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { DetailsContextProvider } from './context/DetailsContext';
 import { App } from './App';
@@ -11,12 +12,16 @@ import './globals.css';
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+const queryClient = new QueryClient();
+
 root.render(
   <StrictMode>
-    <Provider store={store}>
-      <DetailsContextProvider>
-        <App />
-      </DetailsContextProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <DetailsContextProvider>
+          <App />
+        </DetailsContextProvider>
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>
 );
