27b4f5c13b8771af6012938d0ec073ff22ed9a49
propertiess <erofeev2001@yandex.ru>
2023-02-06 18:25:16 +0300

    feat: add current-weather-card component

--- src/features/weather/components/index.ts
+++ src/features/weather/components/index.ts
@@ -1,3 +1,4 @@
+export * from './current-weather-card/CurrentWeatherCard';
 export * from './search-place-form/SearchPlaceForm';
 export * from './weather-card/WeatherCard';
 export * from './weather-container/WeatherContainer';
--- src/features/weather/components/weather-container/WeatherContainer.tsx
+++ src/features/weather/components/weather-container/WeatherContainer.tsx
@@ -1,60 +1,27 @@
-import { Button, Loader } from '@/components';
-import { WeatherCard, WeatherList } from '@/features/weather/components';
+import { Button } from '@/components';
+import { CurrentWeatherCard, WeatherList } from '@/features/weather/components';
 import { useDetailsContext } from '@/features/weather/context';
 import { useGetCurrentDayWeather } from '@/features/weather/hooks';
 
 export const WeatherContainer = () => {
   const { detailsIsOpen, setOpenDetails } = useDetailsContext();
-
-  const {
-    data: currentDayWeather,
-    isError,
-    isFetching
-  } = useGetCurrentDayWeather();
+  const { data: currentWeather } = useGetCurrentDayWeather();
 
   const openDetails = () => {
-    if (!detailsIsOpen) {
-      setOpenDetails(true);
-    }
+    !detailsIsOpen && setOpenDetails(true);
   };
 
-  if (isFetching) {
-    return (
-      <div className='flex justify-center'>
-        <Loader />
-      </div>
-    );
-  }
-
-  if (isError) {
-    return <div className='flex justify-center mt-7'>Ошибка!</div>;
+  if (detailsIsOpen) {
+    return <WeatherList />;
   }
 
   return (
     <>
-      {currentDayWeather?.name && (
-        <h3 className='text-2xl text-center mt-7'>{currentDayWeather?.name}</h3>
-      )}
-      {!detailsIsOpen ? (
-        <div className='flex flex-col items-center gap-3 justify-center mt-7'>
-          {currentDayWeather?.weather.map(weather => {
-            return (
-              <WeatherCard
-                desc={weather.description}
-                day={1}
-                key={weather.id}
-                img={weather.icon}
-                temp={currentDayWeather.main!.temp}
-                date={currentDayWeather.dt!}
-              />
-            );
-          })}
-          {currentDayWeather && (
-            <Button onClick={openDetails}>Прогноз на ближ. дни</Button>
-          )}
-        </div>
-      ) : (
-        <WeatherList />
+      <CurrentWeatherCard />
+      {currentWeather && (
+        <Button className='block mx-auto mt-7' onClick={openDetails}>
+          Прогноз на ближ. дни
+        </Button>
       )}
     </>
   );
